8df275a66fb6edc72ba0f51f856c3b2893b67eb5
Mateusz Jabłoński <mat.w.jablonski@gmail.com>
2019-08-17 21:56:37 +0200

    remove old controllers

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const dotenv = require('dotenv/config');
 const express = require('express');
 const cors = require('cors');
-const postsRouter = require('./controllers/post/posts.controller');
-const postRouter = require('./controllers/post/post.controller');
 
 // import models, { connectDb } from './models';
 const PostsRouter = require('./controllers/post/PostsController');
@@ -26,8 +24,6 @@ app.get('/', (req, res) => {
   res.json('api connected');
 });
 
-app.use('/api/post', postRouter);
-app.use('/api/posts', postsRouter);
 app.use('/api/author', AuthorRouter);
 app.use('/api/users', UserRouter);
 app.use('/api/post', PostRouter);
